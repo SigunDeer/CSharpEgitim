469bbbd92ccbe273d1c35948affac5a5abf91581
Mert KÜÇÜKKAPDAN <mertkucukkapdan@gmail.com>
2025-03-05 18:26:22 +0300

    Add project files.

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Yazdırma komutları
+            //Console.Write("Merhaba Dünya");
+
+            //Console.WriteLine("***** Yemek Kategorileri *****");
+            //Console.WriteLine();
+            //Console.WriteLine("1- Çorbalar");
+            //Console.WriteLine("2- Ana Yemekler");
+            //Console.WriteLine("3- Salatalar");
+            //Console.WriteLine("4- Tatalılar");
+            //Console.WriteLine("5- İçecekler");
+            //Console.WriteLine("***** Yemek Kategorileri *****");
+            #endregion
+
+
+            #region String Değişkenler
+            //string name;
+            //name = "Mert";
+            //Console.WriteLine(name);
+
+            //string customerName;
+            //string customerSurname;
+            //string customerPhone;
+            //string customerEmail, district, city;
+
+            //customerName = "Mert";
+            //customerSurname = "Kaptan";
+            //customerPhone = "0532 123 45 67";
+            //customerEmail = "deneme@gmail.com";
+            //district = "Kadıköy";
+            //city = "İstanbul";
+
+
+            //Console.WriteLine("**** Rezervasyon Kartı ****");
+            //Console.WriteLine();
+            //Console.WriteLine("------------------------------------");
+            //Console.WriteLine("Müşteri: " + customerName + " " + customerSurname);
+            //Console.WriteLine("Telefon: " + customerPhone);
+            //Console.WriteLine("E-Posta: " + customerEmail);
+            //Console.WriteLine("Adres: " + district + " / " + city);
+            //Console.WriteLine("------------------------------------");
+
+
+            //customerName = "Ali";
+            //customerSurname = "kaya";
+            //customerPhone = "0532 123 45 67";
+            //customerEmail = "test@gmail.com";
+            //district = "Kadıköy";
+            //city = "İstanbul";
+            //Console.WriteLine();
+
+            //Console.WriteLine("------------------------------------");
+            //Console.WriteLine("Müşteri: " + customerName + " " + customerSurname);
+            //Console.WriteLine("Telefon: " + customerPhone);
+            //Console.WriteLine("E-Posta: " + customerEmail);
+            //Console.WriteLine("Adres: " + district + " / " + city);
+            //Console.WriteLine("------------------------------------");
+
+            #endregion
+
+
+            #region Int Değişkenler
+
+
+
+            #endregion
+        }
+    }
+}
